a5601b05f56ed31cc7ee8d748f12f35ed1a5c29e
Artur Bodera <abodera@gmail.com>
2023-09-08 11:58:16 +1000

    Fix lint error.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { UserButton, SignedIn, SignedOut } from '@clerk/nextjs'
-import Link from 'next/link'
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -10,30 +9,48 @@ export default function Home() {
         <UserButton />
       </SignedIn>
       <SignedOut>
-        <Link href="/sign-in"><button>Sign In</button></Link>
+        <Link href="/sign-in">
+          <button>Sign In</button>
+        </Link>
       </SignedOut>
       <ul>
         <li>
-          <Link href="/api/public-route"><button>/api/public-route</button></Link>
-          <p>should work all the time and either return the user id or 'no user' in the message</p>
+          <Link href="/api/public-route">
+            <button>/api/public-route</button>
+          </Link>
+          <p>{`should work all the time and either return the user id or 'no user' in the message`}</p>
         </li>
         <li>
-          <Link href="/api/protected-route"><button>/api/protected-route</button></Link>
-          <p>if the user is logged in, return message with userId. otherwise redirect to sign-in</p>
+          <Link href="/api/protected-route">
+            <button>/api/protected-route</button>
+          </Link>
+          <p>
+            if the user is logged in, return message with userId. otherwise
+            redirect to sign-in
+          </p>
         </li>
         <li>
-          <Link href="/api/protected-route-no-sdk"><button>/api/protected-route-no-sdk</button></Link>
+          <Link href="/api/protected-route-no-sdk">
+            <button>/api/protected-route-no-sdk</button>
+          </Link>
           <p>same as above, but no userId.</p>
         </li>
         <li>
-          <Link href="/api/protected-by-route"><button>/api/protected-by-route</button></Link>
-          <p>if user is logged show userId. If not show message user is not logged in and 401 in network tab</p>
+          <Link href="/api/protected-by-route">
+            <button>/api/protected-by-route</button>
+          </Link>
+          <p>
+            if user is logged show userId. If not show message user is not
+            logged in and 401 in network tab
+          </p>
         </li>
         <li>
-          <Link href="/api/ignored-route"><button>/api/ignore-route</button></Link>
+          <Link href="/api/ignored-route">
+            <button>/api/ignore-route</button>
+          </Link>
           <p>always works, never any userId or auth info</p>
         </li>
       </ul>
     </main>
-  )
+  );
 }
